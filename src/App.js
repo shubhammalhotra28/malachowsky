a2d42fa335ca74ba2ed9a793737b56a820bf38df
Steven Hulbert <sdh2110@g.rit.edu>
2022-02-11 09:50:26 -0500

    Adds skeleton page for class rating

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import ChowStatus from './components/ChowStatus';
+import ChowClasses from './components/ChowClasses';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route path="/">
               <Route index element={<Homepage />} />
               <Route path="chowstatus" element={<ChowStatus />} />
+              <Route path="chowclasses" element={<ChowClasses />} />
             </Route>
           </Routes>
         </BrowserRouter>
---
+++ src/components/ChowClasses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ChowClasses extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rate My Chow</h1>
+        <span>WIP Content</span>
+      </div>
+    );
+  }
+}
