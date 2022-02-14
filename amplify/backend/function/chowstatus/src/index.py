0d318e8655b5728e6d97e70695d3257270555e29
shubham <sm9971@rit.edu>
2022-02-14 09:31:36 -0500

    added changes for the chowstatus api with flask

--- amplify/backend/function/chowstatus/src/index.py
+++ amplify/backend/function/chowstatus/src/index.py
@@ -1,15 +1,15 @@
-import json
+import awsgi
+from flask_cors import CORS
+from flask import Flask, jsonify, request
 
-def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+BASE_ROUTE = "/chowstatus"
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route(BASE_ROUTE,methods=["GET"])
+def getStatus():
+    return "hello world"
+
+def handler(event,context):
+    return awsgi.response(app,event,context)
