4d14b45bf961b8beb6be12d4ffff37735c749e58
jared barton <jmb7588@g.rit.edu>
2022-02-14 09:19:17 -0500

    Thug life photo as background

--- src/components/ChowStatus.js
+++ src/components/ChowStatus.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import background from '../resources/tophat_chow.png';
+import { Button} from 'react-bootstrap';
 import styled from 'styled-components';
 import './ChowStatus.css'
 
@@ -12,6 +14,7 @@ class ChowStatus extends React.Component {
 
   handleSubmit(event) {
     alert("The Chow's danger level is: " + this.state.danger + "\r\nThe Chow's mood level is: " + this.state.mood);
+
     event.preventDefault();
   }
 
@@ -26,30 +29,40 @@ class ChowStatus extends React.Component {
   render() {
     return (
       <div class='main'>
-        <h1>Chow Status</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            What is the Chow's current danger level from 1-5, 10 being in imminent danger:
-            <select value={this.state.danger} onChange={this.handleChangeDanger}>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </label>
-          <label>
-            <br /><br />Rate the Chow's current mood from 1-5, 1 is if he's talking to a game design major, and 5 is him reading the agile manifesto:
-            <select value={this.state.mood} onChange={this.handleChangeMood}>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+        <div class="menu">
+            <Button onClick={event =>  window.location.href='/chowmap'}>Chow Map</Button>
+            <Button onClick={event =>  window.location.href='/chowlocation'}>Chow Location</Button>
+            <Button onClick={event =>  window.location.href='/chowphoto'}>Chow Photo</Button>
+            <Button onClick={event =>  window.location.href='/chowstatus'}>Chow Status</Button>
+            <Button onClick={event =>  window.location.href='/chowclasses'}>Chow Classes</Button>
+            <Button onClick={event =>  window.location.href='/chowdash'}>Chow Dash</Button>
+        </div>
+        <div class='status'>
+        <h1 class='header'>Chow Status</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Rate the Chow's current danger level from 1-5:
+              <select value={this.state.danger} onChange={this.handleChangeDanger}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </label>
+            <label>
+              <br /><br />Rate the Chow's current mood from 1-5:
+              <select value={this.state.mood} onChange={this.handleChangeMood}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
