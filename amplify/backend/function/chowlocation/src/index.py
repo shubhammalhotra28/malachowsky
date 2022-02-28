4ca38a7df92376bfd2ba55b7d10b67b61d8a6e6b
chaub2251 <cha2251@g.rit.edu>
2022-02-28 18:08:29 -0500

    Rough backend for location reporting

--- amplify/backend/function/chowlocation/src/index.py
+++ amplify/backend/function/chowlocation/src/index.py
@@ -1,4 +1,5 @@
 import awsgi
+import boto3
 from flask_cors import CORS
 from flask import Flask, jsonify, request
 
@@ -6,25 +7,25 @@ BASE_ROUTE = "/chowlocation"
 
 app = Flask(__name__)
 CORS(app)
+client = boto3.client('dynamodb', region_name='us-east-1')
 
 @app.route(BASE_ROUTE, methods=['GET'])
-def testFunc():
-    response = {
-      'statusCode': 200,
-      'body': 'Test Function',
-      'headers': {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-        },
-    }
-    return response
-
+def getLocations():
+    return jsonify(data=client.scan(TableName="location"))
 
 @app.route(BASE_ROUTE,methods=["POST"])
-def updateLocation():
+def addLocation():
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+
+    client.put_item(TableName="location", Item={
+        'lat': {'S': lat},
+        'lng': {'S': lng},
+    })
+
     response = {
       'statusCode': 200,
-      'body': 'Pos Stored',
+      'body': "Position: "+str(lat)+" "+str(lng),
       'headers': {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
@@ -34,3 +35,6 @@ def updateLocation():
 
 def handler(event,context):
     return awsgi.response(app,event,context)
+
+#{"httpMethod": "POST", "path": "/chowlocation", "queryStringParameters": {"lat":43.0755308,"lng":-77.6698865}}
+#{"httpMethod": "GET", "path": "/chowlocation", "queryStringParameters": ""}
