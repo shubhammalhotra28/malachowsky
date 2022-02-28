c4fbf7d94b3c91c13320ad653468d61d5976b08e
Jrumfelt <jrr2707@rit.edu>
2022-02-28 18:09:06 -0500

    API for chow map

--- amplify/backend/function/chowmap/src/index.py
+++ amplify/backend/function/chowmap/src/index.py
@@ -1,15 +1,32 @@
-import json
+import awsgi
+from flask_cors import CORS
+from flask import Flask, jsonify, request
+import boto3
+from datetime import date
 
-def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/chowMap",methods=["GET"])
+def getLocations():
+    try:
+        #Initialize date as one week ago (7 days)
+        date = date.today() - date.timedelta(days=7)
+        
+        #Set up dynamodb connections
+        client = boto3.client('dynamodb')
+        resource = boto3.resource('dynamodb')
+        table = resource.Table("Location")
+        
+        #Query for reported locations from past 7 days
+        response = table.query(
+            KeyConditionExpression = Key('date').between(date, date.today())
+        )
+        locations = response['Items']
+        
+        return locations
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+
+def handler(event,context):
+    return awsgi.response(app,event,context)
